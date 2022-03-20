b6f9ea10dbcf487045084275b934972881dbb856
Datekyosuke <99591187+Datekyosuke@users.noreply.github.com>
2022-03-20 21:32:15 +0300

    Add files via upload

---
+++ 2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+public class MainClass
+{
+    public static void Main()
+    {
+        Console.WriteLine("!");
+        Console.WriteLine("");
+        Console.WriteLine("№");
+        Console.WriteLine("12");
+        Console.WriteLine("34");
+        Console.WriteLine("56");
+        Console.WriteLine("90");
+    }
+}
