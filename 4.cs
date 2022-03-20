d5e1aea6c418fa4d31444f4629d482217d7b769d
Datekyosuke <99591187+Datekyosuke@users.noreply.github.com>
2022-03-20 21:41:33 +0300

    Add files via upload

---
+++ 4.cs
@@ -0,0 +1,9 @@
+﻿using System;
+public class MainClass
+{
+    public static void Main()
+    {
+        var name = Console.ReadLine();
+        Console.WriteLine($"Лучший фильм 2021 года - {name}");
+    }
+}
