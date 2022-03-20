3a2a714e2d9ffd7cf2bb48e8bac6cfabb2098dd9
Datekyosuke <99591187+Datekyosuke@users.noreply.github.com>
2022-03-20 22:07:05 +0300

    Add files via upload

---
+++ 5.cs
@@ -0,0 +1,14 @@
+﻿using System;
+public class MainClass
+{
+    public static void Main()
+    {
+        string name = "Билл ";
+        string fam = "Гейтс ";
+        string prof = "Предприниматель. ";
+        int data = 28;
+        int age = 1955;
+        Console.WriteLine(name + fam + prof +"Родился " +data+ " октября " + age);
+
+    }
+}
