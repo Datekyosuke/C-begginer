1dfba1913830b0f9c2626e6defdc6ba8751770a2
Datekyosuke <99591187+Datekyosuke@users.noreply.github.com>
2022-03-21 19:47:20 +0300

    Add files via upload

---
+++ 8.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+
+        Console.WriteLine("Законодательство США допускало отправку детей по почте до 1913 года\nОтветь на данный факт true или false");
+        string stroka = Console.RideLine();
+        bool ans = Convert.ToBoolean(stroka);
+        Console.WriteLine("Oтвет: " + ans);
+    }
+}
