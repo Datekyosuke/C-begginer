33dfef94bdb175f49efbc1552f0ee7dd43ea8d89
Datekyosuke <99591187+Datekyosuke@users.noreply.github.com>
2022-03-21 20:36:36 +0300

    Add files via upload

---
+++ 9.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        Console.WriteLine("Введите числа в диапазоне от 1 до 100.");
+        string aa = Console.ReadLine();
+        float a = Convert.ToSingle(aa);
+        string bb = Console.ReadLine();
+        float b = Convert.ToSingle(bb);
+        Console.WriteLine("1 число: " + a);
+        Console.WriteLine("2 число: " + b);
+        float c = a / b;
+        Console.WriteLine(c);
+    }
+}
