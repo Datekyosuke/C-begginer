100a5a9766ef4d5dffc06fc156dfcdecf73c254d
Datekyosuke <99591187+Datekyosuke@users.noreply.github.com>
2022-03-23 21:18:53 +0300

    Add files via upload

---
+++ one yet stupid calc.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        float s = 0;
+        Console.WriteLine("Введите число 1:");
+        float a = Convert.ToSingle(Console.ReadLine());
+        Console.WriteLine("Введите число 2:");
+        float b = Convert.ToSingle(Console.ReadLine());
+        Console.WriteLine("Выберите операцию: '+' '-' '*' '/'");
+        char ch = Convert.ToChar(Console.ReadLine());
+        switch (ch)
+        {
+            case '+':
+                s = a + b;
+                Console.WriteLine("a + b = " + s);
+                break;
+            case '-':
+                s = a - b;
+                Console.WriteLine("a - b = " + s);
+                break;
+            case '*':
+                s = a * b;
+                Console.WriteLine("a * b = " + s);
+                break;
+            case '/':
+                s = a / b;
+                Console.WriteLine("a / b = " + s);
+                break;
+
+            default:
+                Console.WriteLine("Попробуйте еще раз..");
+                break;
+        }
+    }
+}
