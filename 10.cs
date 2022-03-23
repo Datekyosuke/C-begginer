9924277504829c04f48d878b0ccc550b2f11e48d
Datekyosuke <99591187+Datekyosuke@users.noreply.github.com>
2022-03-23 20:45:12 +0300

    Add files via upload

---
+++ 10.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        Console.WriteLine("Введите имя:");
+        string name = Console.ReadLine();
+        Console.WriteLine("Имя ученика:" + name);
+        Console.WriteLine("Введите возраст:");
+        int age = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Возраст ученика:" + age);
+        Console.WriteLine("Введите балл по русскому языку:");
+        int rus = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите балл по математике:");
+        int math = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите балл по информатике:");
+        int inf = Convert.ToInt32(Console.ReadLine());
+        float sr = (rus + math + inf) / 3;
+        Console.WriteLine("Средний балл ученика:" + sr);
+
+    }
+}
